b6cff4e916d1e538687f1194cae365fe6dcee17a
Muhammad-Zohaib6 <zohaibmian119@gmail.com>
2026-02-28 22:01:14 +0500

    admin panel next error

--- admin-panel/lib/api.ts
+++ admin-panel/lib/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 import Cookies from "js-cookie";
 
-const BASE_URL = "http://localhost:3002";
+const BASE_URL = process.env.NEXT_PUBLIC_API_URL || "http://localhost:3002";
 
 const api = axios.create({
   baseURL: BASE_URL,
